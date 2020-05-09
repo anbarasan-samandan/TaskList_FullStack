716c9dbc2d3f49ae6c5aa79372124340c4def239
Anbarasan Samandan <anbarasan.samandan@gmail.com>
2020-05-09 13:07:15 +0530

    First version of the app

---
+++ src/server/defaultState.js
@@ -0,0 +1,67 @@
+export const defaultState = {
+  users: [
+    {
+      id: "U1",
+      name: "Dev",
+    },
+    {
+      id: "U2",
+      name: "Anbu",
+    },
+  ],
+  groups: [
+    {
+      name: "TO DO",
+      id: "G1",
+      owner: "U1",
+    },
+    {
+      name: "Doing",
+      id: "G2",
+      owner: "U1",
+    },
+    {
+      name: "Done",
+      id: "G3",
+      owner: "U1",
+    },
+  ],
+  tasks: [
+    {
+      name: "Do tests",
+      id: "T1",
+      group: "G1",
+      owner: "U1",
+      isComplete: false,
+    },
+    {
+      name: "Learn React",
+      id: "T2",
+      group: "G1",
+      owner: "U1",
+      isComplete: true,
+    },
+    {
+      name: "Create GULP for automation",
+      id: "T3",
+      group: "G2",
+      owner: "U2",
+      isComplete: false,
+    },
+    {
+      name: "Finalize Azure date",
+      id: "T2",
+      group: "G3",
+      owner: "U2",
+      isComplete: true,
+    },
+  ],
+  comments: [
+    {
+      owner: "U1",
+      id: "C1",
+      task: "T1",
+      content: "Great work!",
+    },
+  ],
+};
